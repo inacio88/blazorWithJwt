9628c356894af541f3763404384a0a62b8b452a8
inacio88 <inacio88@outlook.com>
2024-11-04 14:55:04 -0300

    master: apiClient

--- JwtProjeto.Web/ApiClient.cs
+++ JwtProjeto.Web/ApiClient.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using Newtonsoft.Json;
+
 namespace JwtProjeto.Web;
 
 public class ApiClient(HttpClient httpClient)
@@ -5,6 +8,16 @@ public class ApiClient(HttpClient httpClient)
     public Task<T> GetFromJsonAsync<T>(string path)
     {
         return httpClient.GetFromJsonAsync<T>(path);
+    }
+
+    public async Task<T1> PostAsync<T1, T2>(string path, T2 postModel)
+    {
+        var res = await httpClient.PostAsJsonAsync(path, postModel);
+        if (res is not null && res.IsSuccessStatusCode)
+        {
+            return JsonConvert.DeserializeObject<T1>(await res.Content.ReadAsStringAsync());
+        }
+        return default;
     }
 }
 
---
+++ JwtProjeto.Web/Components/Pages/Product/CreateProduct.cs
@@ -0,0 +1,13 @@
+using JwtProjeto.Models.Models;
+
+namespace JwtProjeto.Web.Components.Pages.Product;
+
+public partial class CreateProduct
+{
+    public ProductModel Model { get; set; }
+
+    public async Task Submit()
+    {
+        
+    }
+}
